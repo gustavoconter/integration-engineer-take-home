b9105003d23d5cd6143cde2b630680c4f9341dcd
Gustavo Conte <72617921+gustavoconter@users.noreply.github.com>
2024-07-22 00:48:07 -0300

    fix: alert colors

--- frontend/src/components/Alerts/index.tsx
+++ frontend/src/components/Alerts/index.tsx
@@ -1,4 +1,5 @@
 import { Alert } from "../../types";
+import { mergeClasses } from "../../utils";
 
 const Alerts: React.FC<{
 	alerts: Alert[];
@@ -9,12 +10,17 @@ const Alerts: React.FC<{
 				<section className="fixed bottom-4 right-4 max-w-xs z-10 w-full flex flex-col gap-2 pointer-events-none select-none">
 					{alerts.map((alert, index) => (
 						<div
-							className="flex flex-row gap-2 shadow-lg rounded-md p-4 bg-red-50 border border-red-200"
+							className={mergeClasses([
+								"flex flex-row gap-2 shadow-lg rounded-md p-4 border",
+								alert.type === "error"
+									? "border-red-500 bg-red-50 text-red-600"
+									: "border-green-500 bg-green-50 text-green-600",
+							])}
 							key={index}
 						>
 							<div className="flex-grow flex items-center gap-2">
-								<div className="w-5 h-5 bg-red-400 rounded-md"></div>
-								<span className="text-red-500">{alert.message}</span>
+								<div className="w-5 h-5 bg-current rounded-md animate-pulse"></div>
+								<h4 className="text-current">{alert.message}</h4>
 							</div>
 						</div>
 					))}
